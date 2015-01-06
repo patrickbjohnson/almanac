1b071c596d2a5edc88942b23585f1f939483b66c
Patrick Johnson <im@pbj.me>
2015-01-05 22:49:09 -0500

    committig

---
+++ wp-content/themes/test-theme/footer.php
@@ -0,0 +1,32 @@
+</section>
+<footer class="full">
+	<h1>footer logo with full bleed border</h1>
+	<div class="row">
+		<div class="small-12 medium-12 large-12">
+			<ul class="social">
+				<li>facebook</li>
+				<li>instagram</li>
+				<li>twitter</li>
+			</ul>
+			<div class=vcard>
+				<abbr class="fn org" title="Southern Alberta Institute of Technology">SAIT Polytechnic</abbr>
+				<p class=adr>
+				<span class=street-address>28 Seventh Avenue South</span>
+				<span class=locality>New York</span>
+				<abbr class=region title=Alberta>NY</abbr>
+				<span class=country-name>United States</span>
+				<span class=postal-code>10014</span>
+				<p>P:<a class=tel href="tel:+2122551795">212-255-1795</a>
+				<p>F:<a class=fax href="fax:+2122551720">212-255-1720</a>
+			</div>
+		</div>
+	</div>
+
+	<?php dynamic_sidebar("footer-widgets"); ?>
+
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
