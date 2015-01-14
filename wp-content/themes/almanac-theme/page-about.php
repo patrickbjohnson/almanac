9fe6ee6e152c96b36c5cc69f3ba886371554f73b
Patrick Johnson <im@pbj.me>
2015-01-14 01:05:43 -0500

    finished about page data mockup

--- wp-content/themes/almanac-theme/page-about.php
+++ wp-content/themes/almanac-theme/page-about.php
@@ -35,11 +35,16 @@
 		<?php if ($loop->have_posts()) : while ($loop->have_posts()) : $loop->the_post(); ?>	
 						<?php if( has_term('head-chef', 'staff_role')) :?>
 							<div class="small-12 medium-centered medium-8 large-centerd large-8 columns">
-								<img src="http://placehold.it/700x350">
+								<?php 
+									 $headshot = get_field('chef_picture');
+								?>
+								<img src="<?php echo $headshot['sizes']['large'];?>">
+								<?php 
+								// echo '<pre>';var_dump(get_field('chef_picture')); echo '</pre>';?>
 									<h2 class="team-name"><?php the_title();?></h2>
 									<span class="team-title"><?php the_field('job_title');?></span>
 									
-										<p class="team-bio"><?php the_field('job_bio');?></p>
+									<?php the_field('chef_bio');?>
 									
 							</div>
 						<?php endif;?>
@@ -83,20 +88,23 @@
 			<?php 
 			$location = get_field('almanac_google_map', 'option');
 			$marker 	 = get_field('almanac_restaurant_marker', 'option');
+			$locationString = $location['address'];
+			$address = explode(',', $locationString);
 			if( !empty($location) ):?>
 			<div class="acf-map">
 				<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>" data-marker="<?php echo $marker;?>"></div>
 			</div>
 			<?php endif; ?>
-			<img src="#" alt="">
+			<img src="<?php the_field('almanac_restaurant_logo', 'option');?>" alt="" width="300">
+			
 			<div class=vcard>
-				<abbr class="fn org" title="Almanac NYC">Almanac</abbr>
+				<abbr class="fn org" title="Almanac">Almanac</abbr>
 				<p class=adr>
-				<span class=street-address>28 Seventh Avenue South</span>
-				<span class=locality>New York</span>
-				<abbr class=region title=Alberta>NY</abbr>
-				<span class=country-name>United States</span>
-				<span class=postal-code>10014</span>
+				<span class=street-address><?php echo $address[0];?></span>
+				<span class=locality><?php echo $address[2];?></span>
+				<abbr class=region title=Alberta><?php echo $address[3];?></abbr>
+				<span class=postal-code><?php echo $address[4];?></span>
+				<p><a href="mailto:<?php the_field('almanac_restaurant_email', 'option');?>"><?php the_field('almanac_restaurant_email', 'option');?></a></p>
 				<p>P:<a class=tel href="tel:+2122551795"><?php the_field('almanac_restaurant_phone', 'option');?></a>
 				<p>F:<a class=fax href="fax:+2122551720"><?php the_field('almanac_restaurant_fax', 'option');?></a>
 			</div>
@@ -105,10 +113,8 @@
 			<?php 
 			$location = get_field('mas_google_map', 'option');
 			$marker 	 = get_field('mas_restaurant_marker', 'option');
-			var_dump($location);
-			$address = [];
-			$address = $location['address'].split(',');
-			var_dump($address);
+			$locationString = $location['address'];
+			$address = explode(',', $locationString);
 			if( !empty($location) ):?>
 			<div class="acf-map">
 				<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>" data-marker="<?php echo $marker;?>"></div>
@@ -119,13 +125,13 @@
 			<div class=vcard>
 				<abbr class="fn org" title="Mas Farmhouse">Mas Farmhouse</abbr>
 				<p class=adr>
-				<span class=street-address><?php echo $location['address'];?></span>
-				<span class=locality>New York</span>
-				<abbr class=region title=Alberta>NY</abbr>
-				<span class=country-name>United States</span>
+				<span class=street-address><?php echo $address[1];?></span>
+				<span class=locality><?php echo $address[2];?></span>
+				<abbr class=region title=Alberta><?php echo $address[3];?></abbr>
 				<span class=postal-code>10014</span>
+				<p><a href="mailto:<?php the_field('mas_restaurant_email', 'option');?>"><?php the_field('mas_restaurant_email', 'option');?></a></p>
 				<p>P:<a class=tel href="tel:+2122551795"><?php the_field('mas_restaurant_phone', 'option');?></a>
-								<p>F:<a class=fax href="fax:+2122551720"><?php the_field('mas_restaurant_fax', 'option');?></a>
+				<p>F:<a class=fax href="fax:+2122551720"><?php the_field('mas_restaurant_fax', 'option');?></a>
 			</div>
 		</div>
 	</div>
