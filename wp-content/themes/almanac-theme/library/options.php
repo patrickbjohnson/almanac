41639bf277a9de733c3e54e66f5a85cef66c236e
Patrick Johnson <im@pbj.me>
2015-02-11 16:09:59 -0500

    update header/footer and options page

--- wp-content/themes/almanac-theme/library/options.php
+++ wp-content/themes/almanac-theme/library/options.php
@@ -2,8 +2,8 @@
 
 	if( function_exists('acf_add_options_page') ) {
 		acf_add_options_page(array(
-				'page_title' 	=> 'Restaurants Options',
-				'menu_title'	=> 'Restaurants Options',
+				'page_title' 	=> 'Restaurant Options',
+				'menu_title'	=> 'Restaurant Options',
 				'menu_slug' 	=> 'theme-general-settings',
 				'capability'		=> 'edit_posts',
 				'redirect'		=> false
